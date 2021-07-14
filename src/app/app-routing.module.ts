1006a5adf8c498236ef58d72210afddc46721526
Richard Decker <richarddecker96@gmail.com>
2021-07-14 15:37:45 -0400

    created new components, fixed home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ShirtsComponent } from './shirts/shirts.component';
+import { TopsComponent } from './tops/tops.component';
+import { BottomsComponent } from './bottoms/bottoms.component';
+import { ShoesComponent } from './shoes/shoes.component';
+import { AccesoriesComponent } from './accesories/accesories.component';
+import { HatsComponent } from './hats/hats.component';
 
 const routes: Routes = [
-  {path: "", redirectTo: "/home", pathMatch: "full"},
+  {path: "", redirectTo: "home", pathMatch: "full"},
   {path: "home", component: HomeComponent},
-  {path: "shirts", component: ShirtsComponent }
+  {path: "tops", component: TopsComponent },
+  {path: "bottoms", component: BottomsComponent },
+  {path: "shoes", component: ShoesComponent },
+  {path: "accesories", component: AccesoriesComponent },
+  {path: "hats", component: HatsComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ShirtsComponent } from './shirts/shirts.component';
+import { TopsComponent } from './tops/tops.component';
+import { BottomsComponent } from './bottoms/bottoms.component';
+import { ShoesComponent } from './shoes/shoes.component';
+import { AccesoriesComponent } from './accesories/accesories.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HatsComponent } from './hats/hats.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ShirtsComponent
+    ShirtsComponent,
+    TopsComponent,
+    BottomsComponent,
+    ShoesComponent,
+    AccesoriesComponent,
+    NavBarComponent,
+    HatsComponent
   ],
   imports: [
     BrowserModule,
