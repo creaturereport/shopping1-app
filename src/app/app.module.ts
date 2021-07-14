172ce98a82cd3ba97afc48929e62f3f27e5e30d1
DomWill-byte <78750378+DomWill-byte@users.noreply.github.com>
2021-07-14 18:11:48 -0500

    New-Components for each item | Changed-Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ShirtsComponent } from './shirts/shirts.component';
+import { HatsComponent } from './hats/hats.component';
+import { ShoesComponent } from './shoes/shoes.component';
+import { PantsComponent } from './pants/pants.component';
+import { AccessoriesComponent } from './accessories/accessories.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    routingComponents,
+    InventoryComponent
   ],
   imports: [
     BrowserModule,
