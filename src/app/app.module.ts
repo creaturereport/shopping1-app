efd05eae31c100d856eb981654fda2eb35145e98
Symone Thompson <symone1124thompson@gmail.com>
2021-07-14 14:49:29 -0400

    Routing implented, shirts rendered & styled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ShirtsComponent } from './shirts/shirts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ShirtsComponent
   ],
   imports: [
     BrowserModule,
