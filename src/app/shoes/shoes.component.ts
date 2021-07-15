aa3efa0a490c20f9b2b16b73c99bde5928cf6168
Dustin Bingham <djbingham88@gmail.com>
2021-07-14 19:39:30 -0500

    added dummy data to shoes component

--- src/app/shoes/shoes.component.ts
+++ src/app/shoes/shoes.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoesComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log(this.shoesList)
+   }
 
   ngOnInit(): void {
   }
+
+  shoesList: any = [
+      {
+        id: 7, 
+        name: "Sneakers",
+        price: "120.00",
+        image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBcuecV7DfF2yiQZk0aWPCAGf7yeapUcgMxA&usqp=CAU"
+      }
+  ]
 
 }
