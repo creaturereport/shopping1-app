39b25f5467b7bd8a75260fa6a2226b6497a8151a
Dustin Bingham <djbingham88@gmail.com>
2021-07-14 19:59:59 -0500

    added dummy data to watches component

--- src/app/watches/watches.component.ts
+++ src/app/watches/watches.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WatchesComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log(this.watchesList)
+   }
 
   ngOnInit(): void {
   }
 
+  watchesList: any = [
+    {
+      id: 9, 
+      name: "Watch",
+      price: "180.00",
+      image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjOF4WuAz2_Jlm0daJ9ap0HoIGyBfIgN3bnA&usqp=CAU"
+    }
+  ]
 }
