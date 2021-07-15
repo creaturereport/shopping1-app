98ae848816c71ac5ba8bc2f63631d59d0607792b
Symone Thompson <symone1124thompson@gmail.com>
2021-07-14 22:54:35 -0400

    clean up files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TopsComponent } from './tops/tops.component';
-import { BottomsComponent } from './bottoms/bottoms.component';
-import { HatsComponent } from './hats/hats.component';
 import { PageNotFoundComponent } from './pagenotfound/pagenotfound.component';
-import { ShoesComponent } from './shoes/shoes.component';
-import { AccessoriesComponent } from './accessories/accessories.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 
 
@@ -18,11 +14,9 @@ import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
     AppComponent,
     HomeComponent,
     TopsComponent,
-    BottomsComponent,
-    HatsComponent,
+    
     PageNotFoundComponent,
-    ShoesComponent,
-    AccessoriesComponent,
+   
     BsNavbarComponent,
    
   ],
