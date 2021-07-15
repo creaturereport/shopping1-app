28bdc04f23bd0d527dbed7311d6077f4329d27f3
x3205x <x3205x@gmail.com>
2021-07-15 11:49:09 -0400

    added more routes
    added nav bar and routes other catagories

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HatsComponent } from './hats/hats.component';
 import { HomeComponent } from './home/home.component';
+import { PantsComponent } from './pants/pants.component';
 import { ShirtsComponent } from './shirts/shirts.component';
+import { ShoesComponent } from './shoes/shoes.component';
+import { WatchesComponent } from './watches/watches.component';
 
 const routes: Routes = [
   {path: "", redirectTo: "/home", pathMatch: "full"},
   {path: "home", component: HomeComponent},
-  {path: "shirts", component: ShirtsComponent }
+  {path: "shirts", component: ShirtsComponent },
+  { path: "hats", component: HatsComponent },
+  { path: "shoes", component: ShoesComponent },
+  { path: "pants", component: PantsComponent },
+  { path: "watches", component: WatchesComponent }
 ];
 
 @NgModule({
---
+++ src/app/pants/pants.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pants',
+  templateUrl: './pants.component.html',
+  styleUrls: ['./pants.component.css']
+})
+export class PantsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  pantStyles: any = [
+    {
+      id: 8, 
+     name: "Pant",
+     price: "40.50",
+     image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMmVDrWDGbh2lJO2XSOsidOUpVzwBtaisANA&usqp=CAU"
+   },
+   {
+    id: 5, 
+   name: "Shorts",
+   price: "30.00",
+   image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ3ECe-nEFOYUpEi1K1NqTivc12t7fo2lb5T25Rv-kVXitaI5RA6xo15SlR-Y0kMwriuBximM&usqp=CAc"
+   }, 
+  ]
+
+}
