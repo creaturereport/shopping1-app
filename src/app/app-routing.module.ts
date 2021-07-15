602d4f24819ac9f9cd70bf065a6bd23ce60fe9f4
DomWill-byte <78750378+DomWill-byte@users.noreply.github.com>
2021-07-14 19:40:25 -0500

    Made nav bar and started building other components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccessoriesComponent } from './accessories/accessories.component';
+import { HatsComponent } from './hats/hats.component';
+import { HomeComponent } from './home/home.component';
+import { ShoesComponent } from './shoes/shoes.component';
+import { PantsComponent } from './pants/pants.component';
+import { ShirtsComponent } from './shirts/shirts.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: "", redirectTo: "/home", pathMatch: "full"},
+  {path: "home", component: HomeComponent},
+  {path: "shirts", component: ShirtsComponent },
+  {path: "pants", component: PantsComponent },
+  {path: "hats", component:HatsComponent},
+  {path: "shoes", component:ShoesComponent},
+  {path: "accessories", component: AccessoriesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [HomeComponent,ShirtsComponent,PantsComponent,HatsComponent,AccessoriesComponent]
