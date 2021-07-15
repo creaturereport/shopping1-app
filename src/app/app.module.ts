32b0aa7e44c0efb5bbb5652f91f5d37fc751afa6
Dustin Bingham <djbingham88@gmail.com>
2021-07-14 19:21:28 -0500

    added hats component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ShirtsComponent } from './shirts/shirts.component';
+import { HatsComponent } from './hats/hats.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ShirtsComponent
+    ShirtsComponent,
+    HatsComponent
   ],
   imports: [
     BrowserModule,
