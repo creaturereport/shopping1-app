c9ceed35ae28abd76875033029b88acd5ab29e4c
McknightDa <mcknight0816@gmail.com>
2021-07-16 16:56:02 -0400

    Added Data For First Component

---
+++ src/app/services/products.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Product } from '../classes/product';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  //Define Api Details
+
+  shirtsUrl = 'https://productlist-31b15-default-rtdb.firebaseio.com/Shirts.json'
+  // pantsUrl
+  // watchesUrl
+
+  constructor(private http: HttpClient) { }
+    // Http Options
+    httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }  
+
+    getShirts(): Observable<Product>{
+      return this.http.get<Product>(this.shirtsUrl)
+    }
+    // getPants(): Observable<Product>{
+    //   return this.http.get<Product>(this.shirtsUrl)
+    // }
+}
