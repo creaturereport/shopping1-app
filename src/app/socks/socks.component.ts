e0b6ff5351b2abc2fbd0ef0529f1bc9f45de2719
Symone Thompson <symone1124thompson@gmail.com>
2021-07-17 13:11:24 -0400

    rendered socks

--- src/app/socks/socks.component.ts
+++ src/app/socks/socks.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProductsService } from '../services/products.service';
 @Component({
   selector: 'app-socks',
   templateUrl: './socks.component.html',
   styleUrls: ['./socks.component.css']
 })
 export class SocksComponent implements OnInit {
+  styleSocks: any = []
 
-  constructor() { }
+  constructor(public getSocksApi: ProductsService) { }
 
   ngOnInit(): void {
+    console.log('On Socks Component')
+    this.getAllSocks();
   }
 
+  getAllSocks() {
+    return this.getSocksApi.getSocks().subscribe((data: {}) => {
+      this.styleSocks = data;
+      console.log(this.styleSocks)
+    })
+  }
 }
