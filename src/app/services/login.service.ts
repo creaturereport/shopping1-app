6cb176ed216c7ffdb67489bcb602b3747dd350a3
Symone Thompson <symone1124thompson@gmail.com>
2021-07-19 15:12:34 -0400

    user service created

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  BASE_URL= "https://u2nv-users-default-rtdb.firebaseio.com/"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+   // Http Options
+    //use this for post calls good for forms
+    httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }  
+
+    checkUser(item: string): Observable<any>{
+      return this.http.get<any>(this.BASE_URL + item + ".json")
+    }
 }
