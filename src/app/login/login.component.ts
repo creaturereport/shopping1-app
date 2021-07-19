fb1ce769c9276767553a250a18b3ec45952d84c8
Symone Thompson <symone1124thompson@gmail.com>
2021-07-19 15:29:33 -0400

    validated current users

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { ViewChild } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { LoginService } from '../services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,16 +13,21 @@ export class LoginComponent implements OnInit {
   @ViewChild('f')
     loginForm!: NgForm;
 
-  constructor(public loginApi: LoginService) { }
+  constructor(public loginApi: LoginService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  isValidUser: boolean = false;
+
   onSubmit(form: NgForm){
     return this.loginApi.checkUser(form.value.userName).subscribe((data: {}) => {
-      console.log(data);
-    })
-
+      if(data === null) {
+        this.isValidUser = true;
+      } else {
+          this.router.navigateByUrl('/home');
+        };
+      }
+    )
   }
-
 }
