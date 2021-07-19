be3854ff8e5e3aba49de4dae9c6527b1e9bc92a2
Symone Thompson <symone1124thompson@gmail.com>
2021-07-19 14:55:34 -0400

    creating methods

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
+import { ViewChild } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  @ViewChild('f')
+    loginForm!: NgForm;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(form: NgForm){
+    console.log("testing")
+  }
 }
