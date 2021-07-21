a2d859da6126075ef17bcba2d0b8425dddb3253a
David Whitehead <david_whitehead_234@yahoo.com>
2021-07-20 20:59:19 -0400

    updated login service

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LoginService {
 
-  constructor() { }
+export class LoginService {
+  Users_URL = "https://shopping-app-users-default-rtdb.firebaseio.com/"
+
+  constructor(private http: HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({ 
+      'Content-Type': 'application/json'
+    })
+  }
+    checkUser(item: string): Observable<any>{
+      return this.http.get<any>(this.Users_URL + item + ".json")
+    }   
+
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { LoginService } from '../login.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  @ViewChild('f')
+    loginForm!: NgForm;
 
-  constructor() { }
+  constructor(public loginApi: LoginService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  isValidUser: boolean = false;
+
+  onSubmit(form: NgForm){
+    return this.loginApi.checkUser(form.value.userName).subscribe((data: {}) => {
+      if(data === null) {
+        this.isValidUser = true;
+      }
+      else { 
+        this.router.navigateByUrl('/home');
+      }
+    })
   }
 
 }
