7f7174fa3415e4e03fc5d8b65d25a46ffc39263f
Richard Decker <richarddecker96@gmail.com>
2021-07-22 16:47:27 -0400

    added reactive forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ShirtsComponent } from './shirts/shirts.component';
@@ -10,9 +13,10 @@ import { ShoesComponent } from './shoes/shoes.component';
 import { PantsComponent } from './pants/pants.component';
 import { WatchesComponent } from './watches/watches.component';
 import { NavComponent } from './nav/nav.component';
-import { ProductsService } from './services/products.service';
 import { LoginComponent } from './login/login.component';
-import { FormsModule } from '@angular/forms';
+
+import { ProductsService } from './services/products.service';
+
 
 @NgModule({
   declarations: [
@@ -30,7 +34,8 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [ProductsService],
   bootstrap: [AppComponent]
