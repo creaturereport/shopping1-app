1f1ae3095724f3cd087b9c26726de76a035fdce4
Dustin Bingham <djbingham88@gmail.com>
2021-07-23 13:14:01 -0500

    added profile info and button to display edit form

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -11,9 +11,16 @@ export class ProfileComponent implements OnInit {
 
   constructor(public loginApi: LoginService) { }
   user: User = new User;
+
+  isEditing = false //boolean to determine if user is editing.
+
   ngOnInit(): void {
    this.user = this.loginApi.loggedInUser;
    console.log(this.user)
+  }
+
+  onEdit(){
+    this.isEditing = true;
   }
 
 }
