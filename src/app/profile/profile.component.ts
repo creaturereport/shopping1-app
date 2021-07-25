3b786d7d4bbc6d402c14b073136c92e63cb7ffea
David Whitehead <david_whitehead_234@yahoo.com>
2021-07-24 23:20:41 -0400

    building out profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { User } from '../user';
 import { FormBuilder, Validators } from '@angular/forms';
 import { LoginService } from '../login.service';
 import { NgForm } from '@angular/forms';
@@ -14,16 +15,46 @@ import { Router } from '@angular/router';
 export class ProfileComponent implements OnInit {
 
 
-  constructor(public loginApi: LoginService) { }
-/*   user: User = new User */
+  constructor(public loginApi: LoginService, private formBuilder: FormBuilder) { }
+
+  user: User = new User;
+
+  profileForm = this.formBuilder.group({
+    userName: [""],
+    firstName: [""],
+    lastName: [""],
+    city: [""],
+    email: [""],
+    phoneNumber: [""],
+    birthDate: [""],
+  })
+
+  isEditing = false;
+
   ngOnInit(): void {
+    this.user = this.loginApi.loggedInUser;
+    console.log(this.user)
     
   }
 
-   editProfile () {
-     
+  onEdit(){
+    this.setPlaceholders();
+    this.isEditing = true;
+  }
 
-    }
+  setPlaceholders(){
+    this.profileForm.setValue({
+      userName: this.user.userName,
+      firstName: this.user.firstName,
+      lastName: this.user.lastName,
+      city: this.user.city,
+      email: this.user.email,
+      phoneNumber: this.user.phoneNumber,
+      birthDate: this.user.birthDate,
+
+    })
+  }
+
 
 
   
