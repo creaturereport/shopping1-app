c1b24c8d2d972d57aa6c1547d24b145271c94609
Richard Decker <richarddecker96@gmail.com>
2021-07-26 14:24:25 -0400

    added cart service to pants comp

--- src/app/pants/pants.component.ts
+++ src/app/pants/pants.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart } from '../classes/cart';
+import { CartService } from '../services/cart.service';
 import { ProductsService } from '../services/products.service';
 
 @Component({
@@ -9,7 +11,7 @@ import { ProductsService } from '../services/products.service';
 export class PantsComponent implements OnInit {
   pantStyles: any = [];
 
-  constructor(public getPantsApi: ProductsService) { }
+  constructor(public getPantsApi: ProductsService, public cart: CartService) { }
 
   ngOnInit(): void {
     console.log('on pants comp')
@@ -23,6 +25,40 @@ export class PantsComponent implements OnInit {
     })
   }
 
-  
+  inCartCheck(name: string) {
+    for (let i=0; i < this.cart.cart.length; i++) {
+      if (name === this.cart.cart[i].name) {
+        return true
+      }
+    }
+    return false
+  }
+
+  quantity(name: string) {
+    for (let i = 0; i < this.cart.cart.length; i++) {
+      if (name === this.cart.cart[i].name) {
+        return this.cart.cart[i].quantity
+      }
+    }
+    return 0
+  }
+
+  addItem(price: number, name: string) {
+    let item: Cart = {
+      price: price,
+      name: name,
+      quantity: 1
+    }
+    this.cart.addItem(item)
+  }
+
+  removeItem(name: string, price:number) {
+    let item: Cart = {
+      price: price,
+      name: name,
+      quantity: 1
+    }
+    this.cart.removeItem(item)
+  }
 
 }
