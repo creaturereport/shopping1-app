33f22e77debae5594c0710df0249ad0c40dbbedf
Richard Decker <richarddecker96@gmail.com>
2021-07-26 14:32:19 -0400

    added cart link to nav

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../services/login.service';
 import { User } from '../classes/user';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,12 +10,16 @@ import { User } from '../classes/user';
 })
 export class NavComponent implements OnInit {
   loggedInUser = "";
-  constructor(public loginApi: LoginService) { }
+  constructor(public loginApi: LoginService, public cart: CartService) { }
   ngOnInit(): void {
 
    this.loggedInUser = this.loginApi.loggedInUser.userName;
    console.log(this.loggedInUser);
 
+  }
+
+  uniqueItemCount() {
+    return this.cart.cart.length
   }
 
 }
